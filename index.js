34f7a9f460def370127e611b97e076a8976f4754
Brandon Castro <castroyou999@gmail.com>
2022-09-09 10:59:59 -0400

    commit tree e15491cabcfa75f5d0681d97b85ee717cc37eb2b

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your solution here!
+let cats = ["Milo","Otis","Garfield"];
+function destructivelyAppendCat(name){
+    return cats.push(name)
+};
+function destructivelyPrependCat(name){
+    return cats.unshift(name)
+};
+let destructivelyRemoveLastCat = () => cats.pop();
+let destructivelyRemoveFirstCat = () => cats.shift();
+let appendCat = name => [...cats, name];
+let prependCat = name => [name, ...cats];
+let removeLastCat = () => cats.slice(0,cats.length-1);
+let removeFirstCat = () => cats.slice(1);
+
